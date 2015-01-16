d90e74feedde3f85aee74558b39874aaedc9a76e
Josh McMillan <josh@joshmcmillan.co.uk>
2015-01-16 16:07:45 +0000

    Set minimum Leiningen version

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject love "0.1.0-SNAPSHOT"
   :description "An tiny app that pulls positive Facebook posts into Slack."
   :url "http://example.com/FIXME"
+  :min-lein-version "2.5.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
